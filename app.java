88d137253681af49cf9e44d2c07bc3cc7f5d688e
shiva <shiva@gmail.com>
2026-08-27 14:44:16 +0000

    app.java data added

---
+++ app.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Variables
+        String name = "John";
+        int age = 25;
+
+        // Display values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        // Simple calculation
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
